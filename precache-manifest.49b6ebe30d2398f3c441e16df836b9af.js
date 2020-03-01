d82e75c7cc63a1cafc398e503fcfd35acf3e803c
Ivan Melnychenko <ivmel.ca@gmail.com>
2020-03-01 17:32:48 +0200

    Updates

---
+++ precache-manifest.49b6ebe30d2398f3c441e16df836b9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4706ef12ed10c458b65e9ec94e539e0a",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "fd01d869a83f5ba7954b",
+    "url": "/crwn-clothing/static/css/main.a62a53a2.chunk.css"
+  },
+  {
+    "revision": "f01312b9a63de1eddb09",
+    "url": "/crwn-clothing/static/js/2.88eb8f76.chunk.js"
+  },
+  {
+    "revision": "fd01d869a83f5ba7954b",
+    "url": "/crwn-clothing/static/js/main.554bbef3.chunk.js"
+  },
+  {
+    "revision": "5364f81b88af2792ab14",
+    "url": "/crwn-clothing/static/js/runtime-main.0497d462.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
