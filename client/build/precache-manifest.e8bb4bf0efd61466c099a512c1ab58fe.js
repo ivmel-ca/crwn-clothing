bb07ced5f116e26d1db9005d589af3ad9324f26f
Ivan Melnychenko <ivmel.ca@gmail.com>
2020-05-22 14:04:44 +0300

    seperating the cra app into a client folder, added express server in order to process stripe payments

---
+++ client/build/precache-manifest.e8bb4bf0efd61466c099a512c1ab58fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d126d5d8f9ba4bd5bda6465daa0f4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e7bdd5116c1dd2a3d7d",
+    "url": "/static/css/main.deb75f05.chunk.css"
+  },
+  {
+    "revision": "94df242eba83584c777f",
+    "url": "/static/js/2.3eb27287.chunk.js"
+  },
+  {
+    "revision": "7e7bdd5116c1dd2a3d7d",
+    "url": "/static/js/main.45c9b22c.chunk.js"
+  },
+  {
+    "revision": "29fa081c5eecb206b563",
+    "url": "/static/js/runtime-main.edcf4edf.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_u7J4nLmlc3LmhCgNzQJFu1zi00UsTzaAq2';
 
-   const onToken = token => {
-       console.log(token);
-       alert(`Payment Successful`);
-   };
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        })
+            .then(response => {
+                alert('succesful payment');
+            })
+            .catch(error => {
+                console.log('Payment Error: ', error);
+                alert(
+                    'There was an issue with your payment! Please make sure you use the provided credit card.'
+                );
+            });
+    };
 
     return (
         <StripeCheckout
